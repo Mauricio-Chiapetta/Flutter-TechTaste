ec918815ca20eeda4a1307086f7d5ec43739a927
m.faisca1234 <m.faisca1234@gmail.com>
2025-04-18 03:04:16 +0000

    first commit

---
+++ lib/ui/_core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Color(0xFF202123);
+  static const Color white = Color(0xFFDBD7DF);
+  static const Color mainColor = Color(0xFFFFA559);
+  static const Color lightBackground = Color(0xFF343541);
+}
